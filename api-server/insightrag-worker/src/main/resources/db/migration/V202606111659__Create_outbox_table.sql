e963ede375f8fa79196f07dc29c73f54940d8bf6
hieulc02 <hieulc02.dev@gmail.com>
2026-06-16 00:37:48 +0700

    refactor: update ddl script for document outbox table

--- api-server/insightrag-worker/src/main/resources/db/migration/V202606111659__Create_outbox_table.sql
+++ api-server/insightrag-worker/src/main/resources/db/migration/V202606111659__Create_outbox_table.sql
@@ -1,12 +1,10 @@
 DROP TABLE IF EXISTS worker_schema.document_outbox CASCADE;
 
 CREATE TABLE worker_schema.document_outbox(
-    id bigserial primary key,
+    id uuid primary key,
     aggregate_type varchar(255) not null,
     aggregate_id varchar(255) not null,
     event_type varchar(255) not null,
 
-    payload jsonb not null,
-    status varchar(50) not null,
-    created_at timestamptz default now()
+    payload jsonb not null
 );
