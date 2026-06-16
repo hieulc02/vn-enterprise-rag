e1c0c4b9f9bb8b81425a9b05f6336502df35e8bb
hieulc02 <hieulc02.dev@gmail.com>
2026-06-16 22:58:16 +0700

    refactor: restrict InfrastructureException access modifier to protected

--- api-server/insightrag-worker/src/main/java/com/hieulc/insightragworker/exception/infra/InfrastructureException.java
+++ api-server/insightrag-worker/src/main/java/com/hieulc/insightragworker/exception/infra/InfrastructureException.java
@@ -3,7 +3,7 @@ package com.hieulc.insightragworker.exception.infra;
 import com.hieulc.insightragworker.exception.BaseApplicationException;
 
 public abstract class InfrastructureException extends BaseApplicationException {
-    public InfrastructureException(String message, Throwable cause) {
+    protected InfrastructureException(String message, Throwable cause) {
         super(message, cause);
     }
 }
